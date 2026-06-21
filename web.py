f1244d73a533a0c75d94b376f7ceb2c65cc35cac
KiKi <jhogjhihi@gmail.com>
2026-06-22 01:19:53 +0800

    modify web

--- web.py
+++ web.py
@@ -1,17 +1,22 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-def query_db():
+def query_db(status):
     conn = sqlite3.connect('healthcheck.db')
     cur = conn.cursor()
-    cur.execute('select * from records')
+    if status is not None:
+        cur.execute('select * from records where status = ?', (status,))
+    else:  
+        cur.execute('select * from records')
     rows = cur.fetchall()
     return rows
 
 app = Flask(__name__)
 @app.route('/')
 def home():
-    rows = query_db()
+    status = request.args.get('status')
+    rows = query_db(int(status) if status is not None else None)
     return render_template('index.html', rows=rows)
 
-app.run(port=5000)
+if __name__ == '__main__':
+    app.run(port=5000)
